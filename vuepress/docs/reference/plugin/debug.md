e65533861f7903b7242bf8f16b9ad451667ee035
nauam <nauamvictor@outlook.com>
2021-09-28 10:35:53 -0300

    Arquivos esenciais

---
+++ vuepress/docs/reference/plugin/debug.md
@@ -0,0 +1,11 @@
+# debug
+
+<NpmBadge package="@vuepress/plugin-debug" />
+
+This plugin will add a debug component to your site. The component will only take effect in development mode.
+
+## Install
+
+```bash
+npm i -D @vuepress/plugin-debug@next
+```
---
+++ vuepress/packages/@vuepress/bundler-vite/src/types.ts
@@ -0,0 +1,10 @@
+import { Options as VuePluginOptions } from '@vitejs/plugin-vue'
+import type { InlineConfig } from 'vite'
+
+/**
+ * Options for bundler-vite
+ */
+export interface ViteBundlerOptions {
+  viteOptions?: InlineConfig
+  vuePluginOptions?: VuePluginOptions
+}
