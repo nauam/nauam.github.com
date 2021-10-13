84ff4e39a8eb5d4e8bbcf29f762ba3827902107b
nauam <nauamvictor@outlook.com>
2021-10-13 13:58:09 -0300

    del

--- docs/.vuepress/sidebar-menus/en/introduction.js
+++
@@ -1,9 +0,0 @@
-module.exports = [{
-    title: 'Introduction',
-    collapsable: false,
-    sidebarDepth: 1,
-    children: [
-        '/en/introduction/introduction.md',
-        '/en/introduction/getting-help.md'
-    ],
-}]
--- docs/.vuepress/sidebar-menus/introduction.js
+++
@@ -1,9 +0,0 @@
-module.exports = [{
-    title: 'Introdução',
-    collapsable: false,
-    sidebarDepth: 2,
-    children: [
-        '/introduction/introduction',
-        '/introduction/getting-help'
-    ]
-}]
